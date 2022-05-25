7dec098f6aaad781401a701c248ad1b7d59854e2
Joseph Gambill <joey.gambill94@gmail.com>
2022-05-25 08:18:51 -0400

    added readline

--- Princess in the castle/princess.js
+++ Princess in the castle/princess.js
@@ -1,1 +1,10 @@
-Hello world
+const readline = require("readline-sync");
+
+class player {
+    constructor (name, totalCoins, status, hasStar, setName, gotHit, gotPowerup, addCoin,print) {
+     this.name = name
+    this.totalCoins = totalCoins
+    this.status = true   
+    }
+}
+
