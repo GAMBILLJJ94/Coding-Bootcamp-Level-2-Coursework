463517f072c924188b4d1544ccd6e42c9961be8a
Joseph Gambill <joey.gambill94@gmail.com>
2022-05-28 14:26:24 -0400

    more work on axios todo list

--- Axios TODO Project/axios.js
+++ Axios TODO Project/axios.js
@@ -1,9 +1,20 @@
 function assignResponseToVariable(response){
-    console.log(response.data);
+    showData(response.data);
 }
 
-axios.get('https://swapi.dev/api/people/1').then(assignResponseToVariable)
+axios.get('https://api.vschool.io/joeyg/todo').then(assignResponseToVariable)
 
-axios.get('https://swapi.dev/api/people/1').then(response => {
-    console.log(response.data);
-})
+// axios.get('https://swapi.dev/api/people/1').then(response => {
+//     console.log(response.data);
+// })
+
+function showData(arr){
+    for(let i = 0; i < arr.length; i++){
+        const h1 = document.createElement('h1')
+        h1.textContent = arr[i].title
+        document.body.append(h1)
+        const h2 = document.createElement('h2')
+        h2.textContent = arr[i]._id
+        document.body.append(h2)
+} 
+}
